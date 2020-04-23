4c9018bb7b2fa93e408f3e10c35695fbd18ba8c9
Crowdbotics <team@crowdbotics.com>
2020-04-23 11:52:28 +0000

    Update models

--- dtat_dev_3368/wsgi.py
+++ dtat_dev_3368/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtat_dev_3368.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dtat_dev_3368.settings")
 
 application = get_wsgi_application()
